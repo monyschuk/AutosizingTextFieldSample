596ecbe95fbf6e0aee90459a6af693980eedc574
Mark Onyschuk <mark.onyschuk@gmail.com>
2014-06-14 14:00:15 -0400

    Initial Commit

---
+++ AutosizingTextFieldSample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AutosizingTextFieldSample
+//
+//  Created by Mark Onyschuk on 2014-06-14.
+//  Copyright (c) 2014 Mark Onyschuk. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AutosizingTextFieldSample/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  AutosizingTextFieldSample
+//
+//  Created by Mark Onyschuk on 2014-06-14.
+//  Copyright (c) 2014 Mark Onyschuk. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
